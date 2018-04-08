ae53a8d0d03551bbb8b40a2b44503bac12fcfe9a
harry <xtayicy@gmail.com>
2018-04-08 19:07:50 +0800

    hadoop demo

--- src/harry/hadoop/JobCenter.java
+++ src/harry/hadoop/JobCenter.java
@@ -3,10 +3,12 @@ package harry.hadoop;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -21,16 +23,13 @@ import harry.hadoop.reducer.WordReducer;
 public class JobCenter {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration configuration = new Configuration();
+		configuration.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator",">");
+		
 		Job job = Job.getInstance(configuration);
 		job.setJarByClass(JobCenter.class);
 		
-		job.setInputFormatClass(TextInputFormat.class);
-		Path inputPath = new Path("input");
-		TextInputFormat.addInputPath(job, inputPath );
-		
-		job.setOutputFormatClass(TextOutputFormat.class);
-		Path outputPath = new Path("output");
-		TextOutputFormat.setOutputPath(job, outputPath );
+		setInput(job);
+		setOutput(configuration, job);
 		
 		job.setMapperClass(WordMapper.class);
 		job.setReducerClass(WordReducer.class);
@@ -41,5 +40,23 @@ public class JobCenter {
 		job.setOutputValueClass(IntWritable.class);
 		
 		job.waitForCompletion(true);
+	}
+
+	private static void setOutput(Configuration configuration, Job job) throws IOException {
+		job.setOutputFormatClass(TextOutputFormat.class);
+		Path outputPath = new Path("output");
+		FileSystem fileSystem = FileSystem.get(configuration);
+		if(fileSystem.exists(outputPath)){
+			fileSystem.delete(outputPath, true);
+		}
+		
+		TextOutputFormat.setOutputPath(job, outputPath );
+	}
+
+	private static void setInput(Job job) throws IOException {
+		//job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		Path inputPath = new Path("input");
+		TextInputFormat.addInputPath(job, inputPath );
 	}
 }
--- src/harry/hadoop/mapper/WordMapper.java
+++ src/harry/hadoop/mapper/WordMapper.java
@@ -3,18 +3,17 @@ package harry.hadoop.mapper;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 /**
  * 
  * @author harry
  *
  */
-public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+public class WordMapper extends Mapper<Text, Text, Text, IntWritable>{
 	@Override
-	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+	protected void map(Text key, Text value, Mapper<Text, Text, Text, IntWritable>.Context context)
 			throws IOException, InterruptedException {
 		for (String word : value.toString().split(" ")) {
 			context.write(new Text(word), new IntWritable(1));
